73c2f25df53e6de1d90a2d817362090443ac3d6d
goldshoot0720 <goldshoot0720@gmail.com>
2025-08-16 20:46:22 +0800

    20250816 v2

---
+++ github.bat
@@ -0,0 +1,8 @@
+cd "C:\Users\chbon\Documents\pelicanisstaticsitegeneratorbypython"
+git init
+git add .
+git commit -m "20250816 v2"
+git remote add origin https://github.com/goldshoot0720/pelicanisstaticsitegeneratorbypython.git
+git checkout -b master
+git push -u origin master --force
+pause
---
+++ run.bat
@@ -0,0 +1,4 @@
+cd "C:\Users\chbon\Documents\pelicanisstaticsitegeneratorbypython"
+pelican content
+pelican --listen
+pause
